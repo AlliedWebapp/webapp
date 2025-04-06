acb02ca8db39ca45f0db32ff3b02986474b80e3c
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-07 00:40:31 +0530

    ticket project

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -38,7 +38,7 @@ function Tickets() {
       <div className="tickets">
         <div className="ticket-headings">
           <div>Date</div>
-          <div>Product</div>
+          <div>Project</div>
           <div>Status</div>
           <div></div>
         </div>
