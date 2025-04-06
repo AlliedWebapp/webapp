2f1f8ec1359ca6a5141eff6d6bb8fc8bc480c8a5
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-07 00:50:22 +0530

    table

--- frontend/src/components/TicketItem.jsx
+++ frontend/src/components/TicketItem.jsx
@@ -11,7 +11,7 @@ function TicketItem({ ticket }) {
   return (
     <div className="ticket">
       <div>{new Date(ticket.createdAt).toLocaleString("en-US", options)}</div>
-      <div>{ticket.project}</div>
+      <div>{ticket.projectname}</div>
       <div className={`status status-${ticket.status}`}>{ticket.status}</div>
       <div className="ticket-buttons">
       <Link to={`/ticket/${ticket._id}`} className="btn btn-reverse btn-sm">
