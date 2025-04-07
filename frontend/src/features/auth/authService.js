964ba394242855c6cd97e44859c772519431a73b
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-07 15:46:44 +0530

    register

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -4,7 +4,7 @@ const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(`${API_URL}/api/users/register`, userData, {  // Updated to include /api/users/register
+  const response = await axios.post(`${API_URL}/api/users`, userData, {  // Updated to include /api/users/register
     headers: {
       'Content-Type': 'application/json'
     }
