820938081b75d444e400ceed3adbc3a0a8f2c3f9
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-10 19:02:08 +0530

    final css

--- frontend/src/pages/OtherReports.jsx
+++ frontend/src/pages/OtherReports.jsx
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 import "../index.css"; // Reusing global styles
 
 const OtherReports = () => {
-  return (
-    <div className="report-container">
-      <header className="header center-header">
-        <h2 className="other-reports-heading">Other Reports</h2>
-        <p className="other-reports-subheading">
-          Select the report
-        </p>
-      </header>
-
-
-      <div className="button-wrapper">
-        <Link to="/improvement-report" className="report-button">
-          Continual Improvement Report
-        </Link>
-        <Link to="/maintenance-report" className="report-button">
-          Breakdown Maintenance Cum Corrective Action Report
-        </Link>
+    return (
+      <div className="report-container">
+        <div className="other-reports-header">
+          <h2 className="other-reports-heading">Other Reports</h2>
+          <p className="other-reports-subheading">
+            Select the report 
+          </p>
+        </div>
+  
+        <div className="button-wrapper">
+          <Link to="/improvement-report" className="report-button">
+            CONTINUAL IMPROVEMENT REPORT
+          </Link>
+          <Link to="/maintenance-report" className="report-button">
+            BREAK DOWN MAINTENANCE CUM CORRECTIVE ACTION REPORT
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default OtherReports;
+    );
+  };
+  
+  export default OtherReports;
+  
