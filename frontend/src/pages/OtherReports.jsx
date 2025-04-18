a477e018bce2856ae0d6f3b4ac447532528c4ba7
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-18 23:53:23 +0530

    other reports

--- frontend/src/pages/OtherReports.jsx
+++ frontend/src/pages/OtherReports.jsx
@@ -23,17 +23,16 @@ const OtherReports = () => {
         </div>
      {/* New Section for Viewing Reports */}
   <div className="view-reports-header">
-    <h2 className="view-reports-heading">View Reports</h2>
     <p className="view-reports-subheading">
       Select the report you want to view
     </p>
   </div>
-
   <div className="button-wrapper">
     <Link to="/view-improvement-report" className="report-button">
       VIEW CONTINUAL IMPROVEMENT REPORT
     </Link>
-    <Link to="/view-maintenance-report" className="report-button">
+    <Link to="/view-maintenance-
+    report" className="report-button">
       VIEW BREAK DOWN MAINTENANCE CUM CORRECTIVE ACTION REPORT
     </Link>
   </div>
